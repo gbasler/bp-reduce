c4f0e8065335bf638d950f576979929b89c6b4d5
Gerard Basler <gerard.basler@gmail.com>
2013-11-05 00:36:17 +0100

    wip

--- bp-reduce-core/src/test/scala/bpReduce/frontend/BooleanProgramParserTest.scala
+++ bp-reduce-core/src/test/scala/bpReduce/frontend/BooleanProgramParserTest.scala
@@ -44,19 +44,38 @@ class BooleanProgramParserTest extends BaseSpecification {
       val expr = """((a | (!b)) & (!c) & (!d))""".stripMargin
       val parser = new BooleanProgramParser()
       parser.parseAll(parser.expr, expr) must beLike {
-        case parser.Success(And(And(Or(Var(Sym("a"), Current, NonMixed),Not(Var(Sym("b"), Current, NonMixed))),Not(Var(Sym("c"), Current, NonMixed))),Not(Var(Sym("d"), Current, NonMixed))), _) => ok
+        case parser.Success(And(And(Or(Var(Sym("a"), Current, NonMixed), Not(Var(Sym("b"), Current, NonMixed))), Not(Var(Sym("c"), Current, NonMixed))), Not(Var(Sym("d"), Current, NonMixed))), _) => ok
       }
     }
 
-    "big and" in {
-      val expr = """(((b0_s_le_2 | (!b3_l_eq_s)) & (!b4_0_eq_l) & (!b5_1_eq_l)) | ((!b4_0_eq_l) & (!b5_1_eq_l) & b0_s_le_2 & b3_l_eq_s)) & (((!b4_0_eq_l) & (!b5_1_eq_l) & b0_s_le_2 & b3_l_eq_s) | (*))""".stripMargin
+    "3-arg and" in {
+      val expr = """a & b & c""".stripMargin
       val parser = new BooleanProgramParser()
       parser.parseAll(parser.expr, expr) must beLike {
-        case parser.Success(And(And(Or(Var(Sym("a"), Current, NonMixed),Not(Var(Sym("b"), Current, NonMixed))),Not(Var(Sym("c"), Current, NonMixed))),Not(Var(Sym("d"), Current, NonMixed))), _) => ok
+        case parser.Success(And(And(Var(Sym("a"), Current, NonMixed), Var(Sym("b"), Current, NonMixed)), Var(Sym("c"), Current, NonMixed)), _) => ok
       }
     }
+
+//    "big and" in {
+//      val expr = """(((a | (!b)) & (!c) & (!d)) | ((!c) & (!d) & a & b)) & (((!c) & (!d) & a & b) | (*))""".stripMargin
+//      val parser = new BooleanProgramParser()
+//      parser.parseAll(parser.expr, expr) must beLike {
+//        case parser.Success(
+//
+//
+//        And(Or(And(And(Or(Var(Sym("a"), Current, NonMixed), Not(Var(Sym("b"), Current, NonMixed))),
+//        Not(Var(Sym("c"), Current, NonMixed))), Not(Var(Sym("d"), Current, NonMixed))),
+//        And(
+//        Not(Var(Sym("c"), Current, NonMixed)), Not(Var(Sym("d"), Current, NonMixed)),
+//        Var(Sym("a"), Current, NonMixed), Var(Sym("b"), Current, NonMixed))),
+//        Or(And(
+//        Not(Var(Sym("c"), Current, NonMixed)), Not(Var(Sym("d"), Current, NonMixed)),
+//        Var(Sym("a"), Current, NonMixed), Var(Sym("b"), Current, NonMixed)) ,Nondet)
+//        ), _) => ok
+//      }
+//    }
     "big and in if" in {
-      val expr = """if (((b0_s_le_2 | (!b3_l_eq_s)) & (!b4_0_eq_l) & (!b5_1_eq_l)) | ((!b4_0_eq_l) & (!b5_1_eq_l) & b0_s_le_2 & b3_l_eq_s)) & (((!b4_0_eq_l) & (!b5_1_eq_l) & b0_s_le_2 & b3_l_eq_s) | (*)) then goto l2; fi""".stripMargin
+      val expr = """if (((a | (!b)) & (!c) & (!d)) | ((!c) & (!d) & a & b)) & (((!c) & (!d) & a & b) | (*)) then goto l2; fi""".stripMargin
       val parser = new BooleanProgramParser()
       parser.parseAll(parser.labelledStmt, expr) must beLike {
         case parser.Success(_, _) => ok
@@ -64,7 +83,7 @@ class BooleanProgramParserTest extends BaseSpecification {
     }
     "big and in if2" in {
       val expr =
-        """if ((!b5_1_eq_l)) & T then goto l2; fi""".stripMargin
+        """if ((!d)) & T then goto l2; fi""".stripMargin
       val parser = new BooleanProgramParser()
       parser.parseAll(parser.labelledStmt, expr) must beLike {
         case parser.Success(_, _) => ok
