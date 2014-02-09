56b26cca5d5047856b24fce0075d3b64cf0d397e
Gerard Basler <gerard.basler@gmail.com>
2014-02-09 17:39:10 +0100

    wip

--- bp-reduce-core/src/main/scala/bpReduce/transformations/reduction/ComposedProgramReducer.scala
+++ bp-reduce-core/src/main/scala/bpReduce/transformations/reduction/ComposedProgramReducer.scala
@@ -6,29 +6,49 @@ import bpReduce.ast._
 import bpReduce.ast.Function
 import bpReduce.ast.LabelledStmt
 import bpReduce.ast.Program
+import scala.collection.immutable.Nil
+import scala.annotation.tailrec
 
-case class ComposedProgramReducer(stmtReducer: StmtReducer,
-                                  program: Program) extends ProgramReducer {
+final case class PartitionedFunction(original: Function,
+                                     reduced: Seq[LabelledStmt],
+                                     unreduced: Seq[LabelledStmt]) {
+  def function = original.copy(stmts = reduced ++ unreduced)
+}
 
-  val reduced: Seq[Function] = Seq()
-  val unreduced: Seq[Function] = program.functions
-
-  final case class PartitionedFunction(name: String,
-                                       locals: VariableHolder,
-                                       args: Seq[String],
-                                       returns: Int,
-                                       reduced: Seq[LabelledStmt],
-                                       unreduced: Seq[LabelledStmt])
-
-  object PartitionedFunction {
-    def apply(function: Function) = {
-      import function._
-      new PartitionedFunction(name, locals, args, returns)
-    }
+object PartitionedFunction {
+  def apply(function: Function) = {
+    new PartitionedFunction(original = function, reduced = Seq(), unreduced = function.stmts)
   }
+}
 
-  val inProgress = PartitionedFunction()
+final case class ComposedProgramReducer(stmtReducer: StmtReducer,
+                                        program: Program,
+                                        reduced: Seq[Function],
+                                        unreduced: Seq[Function],
+                                        inProgress: PartitionedFunction) extends ProgramReducer {
 
+  def next = {
+
+    @tailrec
+    def findNextStmt(reduced: List[Function],
+                     unreduced: List[Function],
+                     inProgress: PartitionedFunction): Option[ComposedProgramReducer] = {
+      val newInProgress = if (inProgress.unreduced.isEmpty) {
+        // next function
+        unreduced match {
+          case Nil      =>
+            // last function, no reduction possible
+            None
+          case hd :: tl =>
+            // look at next function
+            findNextStmt(reduced :+ inProgress.function, tl.tail, PartitionedFunction(tl.head))
+        }
+      }
+
+    }
+
+
+  }
 
   // TODO: this will not work for object Stmts! need to find another way...
   // can not use eq since skip is case object not class!
@@ -63,4 +83,20 @@ case class ComposedProgramReducer(stmtReducer: StmtReducer,
   override def reduce = ???
 
   override def advance = ???
+}
+
+object ComposedProgramReducer {
+  def apply(stmtReducer: StmtReducer, program: Program): Option[ComposedProgramReducer] = {
+
+    if (program.functions.isEmpty) {
+      None
+    } else {
+      val reduced: Seq[Function] = Seq()
+      val unreduced: Seq[Function] = program.functions.tail
+
+      val inProgress = PartitionedFunction(program.functions.head)
+
+      Some(new ComposedProgramReducer(stmtReducer, program, reduced, unreduced, inProgress))
+    }
+  }
 }
