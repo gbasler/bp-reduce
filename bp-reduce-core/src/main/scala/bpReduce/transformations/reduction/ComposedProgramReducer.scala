27173f0499cf3ce3ba076a918efbda02d36fda63
Gerard Basler <gerard.basler@gmail.com>
2014-02-09 16:07:45 +0100

    case study

--- bp-reduce-core/src/main/scala/bpReduce/transformations/reduction/ComposedProgramReducer.scala
+++ bp-reduce-core/src/main/scala/bpReduce/transformations/reduction/ComposedProgramReducer.scala
@@ -2,10 +2,33 @@ package bpReduce
 package transformations
 package reduction
 
-import bpReduce.ast.{LabelledStmt, Function, Stmt, Program}
+import bpReduce.ast._
+import bpReduce.ast.Function
+import bpReduce.ast.LabelledStmt
+import bpReduce.ast.Program
 
 case class ComposedProgramReducer(stmtReducer: StmtReducer,
                                   program: Program) extends ProgramReducer {
+
+  val reduced: Seq[Function] = Seq()
+  val unreduced: Seq[Function] = program.functions
+
+  final case class PartitionedFunction(name: String,
+                                       locals: VariableHolder,
+                                       args: Seq[String],
+                                       returns: Int,
+                                       reduced: Seq[LabelledStmt],
+                                       unreduced: Seq[LabelledStmt])
+
+  object PartitionedFunction {
+    def apply(function: Function) = {
+      import function._
+      new PartitionedFunction(name, locals, args, returns)
+    }
+  }
+
+  val inProgress = PartitionedFunction()
+
 
   // TODO: this will not work for object Stmts! need to find another way...
   // can not use eq since skip is case object not class!
@@ -17,21 +40,21 @@ case class ComposedProgramReducer(stmtReducer: StmtReducer,
 
     val copiedSoFar =
 
-    for {
-      f <- program.functions
-    } yield {
-      val stmts: Seq[LabelledStmt] = for {
-        l@LabelledStmt(stmt, labels) <- f.stmts
+      for {
+        f <- program.functions
       } yield {
-        if (stmtReducer.isDefinedAt(stmt)) {
-          LabelledStmt(s, labels)
-        } else {
-          l
+        val stmts: Seq[LabelledStmt] = for {
+          l@LabelledStmt(stmt, labels) <- f.stmts
+        } yield {
+          if (stmtReducer.isDefinedAt(stmt)) {
+            LabelledStmt(s, labels)
+          } else {
+            l
+          }
         }
-      }
 
-      f.copy(stmts = stmts)
-    }
+        f.copy(stmts = stmts)
+      }
   }
 
 
