efb98c91fdae782a00fb6ef97242a199abe80e4c
Gerard Basler <gerard.basler@gmail.com>
2014-02-09 16:03:08 +0100

    nope

--- bp-reduce-core/src/main/scala/bpReduce/transformations/reduction/ComposedProgramReducer.scala
+++ bp-reduce-core/src/main/scala/bpReduce/transformations/reduction/ComposedProgramReducer.scala
@@ -2,13 +2,38 @@ package bpReduce
 package transformations
 package reduction
 
-import bpReduce.ast.Program
+import bpReduce.ast.{LabelledStmt, Function, Stmt, Program}
 
 case class ComposedProgramReducer(stmtReducer: StmtReducer,
                                   program: Program) extends ProgramReducer {
 
   // TODO: this will not work for object Stmts! need to find another way...
+  // can not use eq since skip is case object not class!
   val candidates = program.filter(stmtReducer.isDefinedAt)
+
+  val reductions = Seq[Stmt => Program]()
+
+  val r = (s: Stmt) => {
+
+    val copiedSoFar =
+
+    for {
+      f <- program.functions
+    } yield {
+      val stmts: Seq[LabelledStmt] = for {
+        l@LabelledStmt(stmt, labels) <- f.stmts
+      } yield {
+        if (stmtReducer.isDefinedAt(stmt)) {
+          LabelledStmt(s, labels)
+        } else {
+          l
+        }
+      }
+
+      f.copy(stmts = stmts)
+    }
+  }
+
 
   override def current = ???
 
