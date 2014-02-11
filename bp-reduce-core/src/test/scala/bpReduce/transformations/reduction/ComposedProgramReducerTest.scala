1a3d442327704b9004d413061791c9b436d1c2f6
basg <gerard.basler@leonteq.com>
2014-02-11 18:08:40 +0100

    wip

--- bp-reduce-core/src/test/scala/bpReduce/transformations/reduction/ComposedProgramReducerTest.scala
+++ bp-reduce-core/src/test/scala/bpReduce/transformations/reduction/ComposedProgramReducerTest.scala
@@ -59,5 +59,46 @@ class ComposedProgramReducerTest extends Specification {
       reducer.reduce must beNone
       reducer.advance must beNone
     }
+
+    "complex one line program" in {
+      // now the fun starts...
+      val factory = new StmtReducerFactory {
+        override def create(stmt: Stmt): StmtReducer = new StmtReducer {
+
+          override def current = Some(Skip)
+
+          override def reduce = None
+
+          override def advance = None
+        }
+      }
+
+      val program: Program =
+        """|void main()
+          |begin
+          |return;
+          |end
+          |
+        """.stripMargin
+
+      val skip: Program =
+        """|void main()
+          |begin
+          |skip;
+          |end
+          |
+        """.stripMargin
+
+      val reducer: ComposedProgramReducer = ComposedProgramReducer(factory, program).get
+      reducer.current.get must be_==(skip)
+      reducer.reduce must beNone
+      reducer.advance must beNone
+      ok
+    }
+
+    "two line program" in {
+      // now the fun starts...
+      ok
+    }
   }
 }
